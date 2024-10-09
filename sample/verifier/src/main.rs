120b840c08e6a190bc226272ee1c6f8b2c5ee51e
Christian Paquin <cpaquin@microsoft.com>
2024-10-09 23:34:05 +0200

    updated missing files

--- sample/verifier/src/main.rs
+++ sample/verifier/src/main.rs
@@ -14,14 +14,24 @@ struct ProofInfo {
     issuer: String
 }
 
+// route to serve the login page
+#[get("/login")]
+fn login_page() -> Template {
+    Template::render("login", context! {})
+}
+
 // route to verify a ZK proof given a ProofInfo, return a status  
 #[post("/verify", format = "json", data = "<proof_info>")]
 fn verify(proof_info: Json<ProofInfo>) -> Json<String> {
     let status = "valid".to_string();
     Json(status)
+
+    // TODO: redirect to the resource page if the proof is valid
 }
 
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![verify])
+    rocket::build().mount("/", routes![login_page, verify])
+    .attach(Template::fairing())
+
 }
