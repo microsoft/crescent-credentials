8a6acb64b3a009f693c8f7a3e0db4991dbed0b69
Christian Paquin <cpaquin@microsoft.com>
2024-10-29 17:27:17 -0400

    download the issuer pub key

--- sample/client_helper/setup_client_helper.sh
+++ sample/client_helper/setup_client_helper.sh
@@ -2,7 +2,7 @@
 
 # Define the source and target directories
 SOURCE_DIR="../../creds/test-vectors/rs256"
-TARGET_DIR="./data/creds"
+TARGET_DIR="./data/creds/shared"
 
 echo "Copying $SOURCE_DIR to $TARGET_DIR"
 
@@ -11,5 +11,8 @@ mkdir -p "$TARGET_DIR"
 
 # Copy the contents from the source directory to the target directory
 cp -r "$SOURCE_DIR/"* "$TARGET_DIR"
+# Remove unnecessary files (TODO: any other?)
+rm "$TARGET_DIR/token.jwt"
+rm "$TARGET_DIR/issuer.pub"
 
 echo "Done"
