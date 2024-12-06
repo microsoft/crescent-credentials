609c2ab1a2fbbeacc54846de728c427b807d5e4f
Greg Zaverucha <gregz@microsoft.com>
2024-12-05 19:29:18 -0800

    Add script with clippy command
    Signed-off-by: Greg Zaverucha <gregz@microsoft.com>

---
+++ clippy_command.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+# Invoke clippy with this command to allow some lints
+
+cargo clippy --tests -- \
+    -A clippy::needless_range_loop \
+    -A clippy::same_item_push \
+    -A clippy::should_implement_trait \
+
