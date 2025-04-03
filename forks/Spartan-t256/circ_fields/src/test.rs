5d8b6aaa4eb642e5140a3a919de4f701ff13ea1e
Larry Joy <ljoy@microsoft.com>
2025-04-03 12:05:01 -0700

    Replace rug crate with num-bigint

--- forks/Spartan-t256/circ_fields/src/test.rs
+++ forks/Spartan-t256/circ_fields/src/test.rs
@@ -1,7 +1,11 @@
 use super::*;
 use rand::thread_rng;
 use rand::Rng;
-use rug::ops::RemRounding;
+use num_bigint::BigInt as Integer;
+use num_integer::Integer as NumInteger;
+use num_traits::{One, Signed};
+use num_primes::Verification;
+use num_bigint_02::BigUint as BigUint02;
 
 #[test]
 fn inline_signed_bits() {
@@ -32,7 +36,6 @@ fn inline_signed_bits() {
         64
     );
 }
-use rug::Integer;
 
 #[test]
 fn inline_signed_bits_randomized() {
@@ -44,21 +47,28 @@ fn inline_signed_bits_randomized() {
         let big_i = Integer::from(i);
         assert_eq!(
             InlineFieldV(i, InlineFieldTag::Bls12381).signed_bits(),
-            big_i.signed_bits(),
+            signed_bits(&big_i),
             "wrong answer on {:b}",
             i
         )
     }
 }
 
+fn signed_bits(x: &Integer) -> u32 {
+    if x.is_negative() {
+        (-(x + Integer::one())).bits() as u32 + 1
+    } else {
+        x.bits() as u32 + 1
+    }
+}
+
 /// Samples a random integer with up to `max_bits` bits.
 ///
 /// A number with `i` is chosen with probability proportional to `2^-i`.
-fn random_rug_int_exp(rng: &mut impl Rng, max_bits: u32) -> Integer {
+fn random_bigint_exp(rng: &mut impl Rng, max_bits: u32) -> Integer {
+    use num_bigint::RandBigInt;
     let num_bits = rng.gen_range(1u32..max_bits);
-    let mut rug_rng = rug::rand::RandState::new_mersenne_twister();
-    rug_rng.seed(&Integer::from(rng.next_u64()));
-    Integer::from(Integer::random_bits(num_bits, &mut rug_rng))
+    rng.gen_biguint(num_bits as u64).into() // into a non-negative BigInt
 }
 
 /// Sample a [FieldT] that is:
@@ -66,28 +76,51 @@ fn random_rug_int_exp(rng: &mut impl Rng, max_bits: u32) -> Integer {
 /// * Bn w/ p = 0.25
 /// * An integer field otherwise
 ///   * with a number of bits sampled uniformly between 1..max_bits
-fn sample_field_t(r: &mut impl Rng, max_bits: u32) -> FieldT {
+pub fn sample_field_t(r: &mut impl Rng, max_bits: u32) -> FieldT {
+    
     if r.gen_bool(0.5) {
-        if r.gen_bool(0.5) {
+        let result = if r.gen_bool(0.5) {
             FieldT::FBls12381
         } else {
             FieldT::FBn254
-        }
+        };
+        result
     } else {
-        FieldT::IntField(Arc::new(random_rug_int_exp(r, max_bits).next_prime()))
+        let result = FieldT::IntField(Arc::new(next_prime(random_bigint_exp(r, max_bits))));        
+        result
     }
 }
 
-/// Sample a [FieldV]. If `ty` is inlinable, the value will be inline with probability 0.5.
+fn next_prime(n: Integer) -> Integer {
+
+    let mut candidate = n;
+
+    if candidate.is_even() {
+        candidate += 1u8;
+    }
+
+    // is_prime below will fail if candidate < 4
+    if candidate < 4u8.into() {
+        candidate = 5u8.into();
+    }
+
+    while !Verification::is_prime(&BigUint02::from_bytes_le(&candidate.to_bytes_le().1)) {
+        candidate += 2u8;
+    }
+
+    candidate
+}
+
 fn sample_field_v(ty: &FieldT, r: &mut impl Rng) -> FieldV {
     if let Some(t) = ty.inline_tag() {
         if r.gen_bool(0.5) {
             let num_bits = r.gen_range(0..62);
             let i: i64 = r.gen();
-            return FieldV::from(InlineFieldV(i % (1 << num_bits as i64), t));
+            return FieldV::from(InlineFieldV(i % (1 << num_bits), t));
         }
     }
-    ty.new_v(random_rug_int_exp(r, ty.modulus().significant_bits()))
+    let i = random_bigint_exp(r, ty.modulus().bits() as u32);
+    ty.new_v(i)
 }
 
 #[test]
@@ -102,22 +135,22 @@ fn random() {
 
         // add
         let c = a.clone() + &b;
-        let c_i = (a_i.clone() + &b_i).rem_floor(f.modulus());
+        let c_i = (a_i.clone() + &b_i).mod_floor(f.modulus());
         assert_eq!(c.i(), c_i);
 
         // sub
         let c = a.clone() - &b;
-        let c_i = (a_i.clone() - &b_i).rem_floor(f.modulus());
+        let c_i = (a_i.clone() - &b_i).mod_floor(f.modulus());
         assert_eq!(c.i(), c_i);
 
         // mul
         let c = a.clone() * &b;
-        let c_i = (a_i.clone() * &b_i).rem_floor(f.modulus());
+        let c_i = (a_i.clone() * &b_i).mod_floor(f.modulus());
         assert_eq!(c.i(), c_i);
 
         // neg
         let c = -a.clone();
-        let c_i = (-a_i.clone()).rem_floor(f.modulus());
+        let c_i = (-a_i.clone()).mod_floor(f.modulus());
         assert_eq!(c.i(), c_i);
     }
 }
