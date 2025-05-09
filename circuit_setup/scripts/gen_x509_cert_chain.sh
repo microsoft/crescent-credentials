46f1cb67675c36ac7355692a027302daae72f534
Larry Joy <ljoy@microsoft.com>
2025-05-09 13:18:42 -0700

    fix failures of x509 gen on windows

--- circuit_setup/scripts/gen_x509_cert_chain.sh
+++ circuit_setup/scripts/gen_x509_cert_chain.sh
@@ -12,17 +12,26 @@ tmpdir=../generated_files/mdl1
 outdir=../inputs/mdl1
 mkdir -p "$tmpdir"
 
+# prevent gitbash path expansion
+export MSYS_NO_PATHCONV=1
+
 # generate self-signed root CA cert
-openssl req -x509 -new -newkey ec:<(openssl ecparam -name secp521r1) -keyout "$tmpdir/root_CA.key" -out "$tmpdir/root_CA.crt" -nodes -subj "/CN=NY DMV Test Root CA" -days 3650 -config openssl_ca.cnf -extensions v3_ca -sha512
+openssl ecparam -name secp521r1 -out tmpkey.pem
+openssl req -x509 -new -newkey ec:tmpkey.pem -keyout "$tmpdir/root_CA.key" -out "$tmpdir/root_CA.crt" -nodes -subj "/CN=NY DMV Test Root CA" -days 3650 -config openssl_ca.cnf -extensions v3_ca -sha512
+rm tmpkey.pem
 
 # generate intermediate CA cert request
-openssl req -new -newkey ec:<(openssl ecparam -name secp384r1) -keyout "$tmpdir/CA.key" -out "$tmpdir/CA.csr" -nodes -subj "/CN=NY DMV Test CA" -config openssl_ca.cnf -extensions v3_ca -sha384
+openssl ecparam -name secp384r1 -out tmpkey.pem
+openssl req -new -newkey ec:tmpkey.pem  -keyout "$tmpdir/CA.key" -out "$tmpdir/CA.csr" -nodes -subj "/CN=NY DMV Test CA" -config openssl_ca.cnf -sha384
+rm tmpkey.pem
 
 # root CA signs the CA cert request
 openssl x509 -req -in "$tmpdir/CA.csr" -out "$tmpdir/CA.crt" -CA "$tmpdir/root_CA.crt" -CAkey "$tmpdir/root_CA.key" -CAcreateserial -days 1825 -extfile openssl_ca.cnf -extensions v3_ca -sha512
 
 # generate signer cert request
-openssl req -new -newkey ec:<(openssl ecparam -name prime256v1) -keyout "$tmpdir/issuer.key" -out "$tmpdir/issuer.csr" -nodes -subj "/CN=NY DMV Test Issuer" -config openssl_ca.cnf -extensions v3_signer -sha256
+openssl ecparam -name prime256v1 -out tmpkey.pem
+openssl req -new -newkey ec:tmpkey.pem  -keyout "$tmpdir/issuer.key" -out "$tmpdir/issuer.csr" -nodes -subj "/CN=NY DMV Test Issuer" -config openssl_ca.cnf -sha256
+rm tmpkey.pem
 
 # intermediate CA signs the issuer cert request
 openssl x509 -req -in "$tmpdir/issuer.csr" -out "$tmpdir/issuer.crt" -CA "$tmpdir/CA.crt" -CAkey "$tmpdir/CA.key" -CAcreateserial -days 365 -extfile openssl_ca.cnf -extensions v3_signer -sha384
