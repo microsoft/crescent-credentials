973887301a129d8f3a18edc44e0c37256deea6ae
Christian Paquin <cpaquin@microsoft.com>
2025-06-25 12:40:55 -0400

    updated sample

--- sample/client_helper/src/main.rs
+++ sample/client_helper/src/main.rs
@@ -311,12 +311,12 @@ async fn show<'a>(cred_uid: String, disc_uid: String, challenge: String, proof_s
             };
 
             // create the show proof
-            let show_proof =
             if &client_state.credtype == "mdl" {
                 let age = disc_uid_to_age(&disc_uid).map_err(|_| "Disclosure UID does not have associated age parameter".to_string())? as u64;
                 proof_spec.range_over_year = Some(std::collections::BTreeMap::from([("birth_date".to_string(), age)]));
             }
-            create_show_proof(&mut client_state, &range_pk, &io_locations, &proof_spec, device_signature).map_err(|e| format!("Failed to create show proof. {:?}", e))?
+            let show_proof = create_show_proof(&mut client_state, &range_pk, &io_locations, &proof_spec, device_signature)
+                .map_err(|e| format!("Failed to create show proof. {:?}", e))?;
 
             // Return the show proof as a base64-url encoded string
             let show_proof_b64 = write_to_b64url(&show_proof);     
