70251233480ef0d79019e6b7cdab0602dea1547e
Greg Zaverucha <gregz@microsoft.com>
2025-08-06 16:55:44 -0700

    Update clippy_command.sh

--- clippy_command.sh
+++ clippy_command.sh
@@ -7,6 +7,6 @@ RUSTFLAGS="--deny warnings" cargo clippy --release --tests -- \
     -A clippy::needless_range_loop \
     -A clippy::same_item_push \
     -A clippy::should_implement_trait \
-    -A clippy::result_large_err
+    -A clippy::result_large_err \
     -A clippy::uninlined_format_args
 
