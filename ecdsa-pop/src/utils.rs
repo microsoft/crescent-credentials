4e9842d12745427561b85fbb9ba78102d0553a88
Greg Zaverucha <gregz@microsoft.com>
2026-09-18 09:48:44 -0700

    Fix clippy issues in ecdsa-pop and mdl-tools
    Signed-off-by: Greg Zaverucha <gregz@microsoft.com>

--- ecdsa-pop/src/utils.rs
+++ ecdsa-pop/src/utils.rs
@@ -467,7 +467,7 @@ pub fn ff_to_big<FF: ff::PrimeField>(i : &FF) -> BigUint {
 }
 /// converts a hex-encoded string into a BigUint
 pub fn hex_to_big(hex: &str) -> BigUint {
-  let hex = if hex.len() % 2 != 0 {
+  let hex = if !hex.len().is_multiple_of(2) {
     &format!("0{hex}")
   } else {
     hex
